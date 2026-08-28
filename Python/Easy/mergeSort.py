0f1aff17689493e48599caf3683263c7c30b78df
YanoF <frankyyano@gmail.com>
2026-08-28 21:40:24 +0200

    merge sort two lists fix

--- Python/Easy/mergeSort.py
+++ Python/Easy/mergeSort.py
@@ -1,8 +1,12 @@
+from typing import Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         # Create a dummy node to act as the starting point of the merged list
