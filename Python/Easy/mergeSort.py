6a85cec4f2c4d3f87eb658c94fdac228164e6333
YanoF <frankyyano@gmail.com>
2026-08-28 21:31:24 +0200

    merge sort two lists

---
+++ Python/Easy/mergeSort.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # Create a dummy node to act as the starting point of the merged list
+        dummy = ListNode()
+        current = dummy
+        
+        # Traverse both lists as long as neither is empty
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            # Move the current pointer forward
+            current = current.next
+            
+        # At least one of the lists is now empty. 
+        # Attach the remaining nodes of the other list.
+        current.next = list1 if list1 else list2
+        
+        # The merged list starts at dummy.next
+        return dummy.next
